1640af7976a01b116871b36382c797a5546a65ee
yuya.y <20015114452y@gmail.com>
2021-02-10 17:30:29 +0900

    応用前まで完成

--- Instagram/HomeViewController.swift
+++ Instagram/HomeViewController.swift
@@ -55,6 +55,7 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
         }
     }
     
+    //ホーム画面が閉じられる時に呼ばれるメソッド
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         print("DEBUG_PRINT: viewWillDisappear")
@@ -71,6 +72,37 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! PostTableViewCell
         cell.setPostData(postArray[indexPath.row])
         
+        //セル内のボタンをアクションをソースコードで設定する
+        cell.likeButton.addTarget(self, action:#selector(handleButton(_:forEvent:)), for: .touchUpInside)
+        
         return cell
+    }
+    
+    @objc func handleButton(_ sender: UIButton, forEvent event: UIEvent) {
+        print("DEBUG_PRINT: likeボタンがタップされました")
+        
+        //タップされたセルのインデックスを求める
+        let touch = event.allTouches?.first
+        let point = touch!.location(in: self.tableView)
+        let indexPath = tableView.indexPathForRow(at: point)
+        
+        //配列からタップされたセルのインデックスのデータを取り出す
+        let postData = postArray[indexPath!.row]
+        
+        //likesを更新する
+        if let myid = Auth.auth().currentUser?.uid {
+            //更新データを作成する
+            var updateValue: FieldValue
+            if postData.isLiked {
+                //すでにいいねを押した場合は、いいね解除のためmyidを取り除く更新データを作成
+                updateValue = FieldValue.arrayRemove([myid])
+            } else {
+                //今回新たにいいねを押した場合は、myidを追加する更新データを作成
+                updateValue = FieldValue.arrayUnion([myid])
+            }
+            //likesに更新データを書き込む
+            let postRef = Firestore.firestore().collection(Const.PostPath).document(postData.id)
+            postRef.updateData(["likes": updateValue])
+        }
     }
 }
